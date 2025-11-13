591d1d3c89f3b173faf48aaa67b427c3919e25f9
heidihekk <heidi.hekkala@centria.fi>
2025-11-13 13:07:34 +0200

    Lisätty Viikko3

---
+++ viikko3/lue_varaukset.py
@@ -0,0 +1,92 @@
+"""
+Ohjelma joka lukee tiedostossa olevat varaustiedot
+ja tulostaa ne konsoliin käyttäen funkitoita.
+Alla esimerkkitulostus:
+
+Varausnumero: 123
+Varaaja: Anna Virtanen
+Päivämäärä: 31.10.2025
+Aloitusaika: 10.00
+Tuntimäärä: 2
+Tuntihinta: 19,95 €
+Kokonaishinta: 39,9 €
+Maksettu: Kyllä
+Kohde: Kokoustila A
+Puhelin: 0401234567
+Sähköposti: anna.virtanen@example.com
+
+"""
+from datetime import datetime
+
+def hae_varausnumero(varaus):
+    varausnumero = int(varaus[0])
+    print(f"Varausnumero: {varausnumero}")
+
+def hae_varaaja(varaus):
+    nimi = varaus[1]
+    print(f"Varaaja: {nimi}")
+
+def hae_paiva(varaus):
+    pvm = datetime.strptime(varaus[2], "%Y-%m-%d").date()
+    print(f"Päivämäärä: {pvm.strftime("%d.%m.%Y")}")
+
+def hae_aloitusaika(varaus):
+    aika = datetime.strptime(varaus[3], "%H:%M").time()
+    print(f"Aloitusaika: {aika.strftime("%H.%M")}")
+
+def hae_tuntimaara(varaus):
+    tuntimaara = int(varaus[4])
+    print(f"Tuntimäärä: {tuntimaara}")
+
+def hae_tuntihinta(varaus):
+    tuntihinta = float(varaus[5])
+    print(f"Tuntihinta: {tuntihinta:.2f}".replace(".", ","), "€") 
+
+def laske_kokonaishinta(varaus):
+    kokonaishinta = int(varaus[4]) * float(varaus[5])
+    print(f"Kokonaishinta: {kokonaishinta:.2f}".replace(".", ","), "€")
+    
+def hae_maksettu(varaus):
+    maksettu = varaus[6].strip() == "True"
+    print(f"Maksettu: {"Kyllä" if maksettu else "Ei"}") 
+
+def hae_kohde(varaus):
+    kohde = varaus[7]
+    print(f"Kohde: {kohde}")
+
+def hae_puhelin(varaus):
+    puhelin = varaus[8]
+    print(f"Puhelin: {puhelin}")
+
+def hae_sahkoposti(varaus):
+    email = varaus[9]
+    print(f"Sähköposti: {email}")
+
+
+def main():
+    # Maaritellaan tiedoston nimi suoraan koodissa
+    varaukset = "varaukset.txt"
+
+    # Avataan tiedosto, luetaan ja splitataan sisalto
+    with open(varaukset, "r", encoding="utf-8") as f:
+        varaus = f.read().strip()
+        varaus = varaus.split('|')
+
+    # Toteuta loput funktio hae_varaaja(varaus) mukaisesti
+    # Luotavat funktiota tekevat tietotyyppien muunnoksen
+    # ja tulostavat esimerkkitulosteen mukaisesti
+
+    hae_varausnumero(varaus)
+    hae_varaaja(varaus)
+    hae_paiva(varaus)
+    hae_aloitusaika(varaus)
+    hae_tuntimaara(varaus)
+    hae_tuntihinta(varaus)
+    laske_kokonaishinta(varaus)
+    hae_maksettu(varaus)
+    hae_kohde(varaus)
+    hae_puhelin(varaus)
+    hae_sahkoposti(varaus)
+
+if __name__ == "__main__":
+    main()
